4d169e1a03788ae4907293f2c24069e54b2ff3de
piuskrisantono <pius.krisantono@gmail.com>
2020-06-26 14:59:23 +0700

    aselole joss

---
+++ public/precache-manifest.df9f9a1027b2210219b6785e55384c7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b82458d025879915b0f56242193e709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53cee496396dcd7f8a2c",
+    "url": "/static/js/2.507c2563.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/static/js/2.507c2563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2083ed6277e35f57ee91",
+    "url": "/static/js/main.d6adebaa.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
